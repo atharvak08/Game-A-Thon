920a534a9f0b0f094dee606bd7f1e34edcefb1e4
Atharv Kevalram <atharvkevalram@gmail.com>
2021-07-13 22:47:06 +0530

    Added message in sum.c

--- sum.c
+++ sum.c
@@ -9,6 +9,6 @@ int main() {
     // calculating sum
     sum = number1 + number2;      
     
-    printf("%d + %d = %d", number1, number2, sum);
+    printf("The sum is %d + %d = %d", number1, number2, sum);
     return 0;
 }
